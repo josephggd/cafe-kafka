cd0605b3af487b538e4ec614d23f66b04bd2cce4
Joseph G Gonzalez Diaz <joseph.gonzalez_diaz@us.af.mil>
2022-08-13 19:23:56 -0500

    dedupe deps

--- src/main/java/com/jsjavaprojects/kafkapractice/CKApplication.java
+++ src/main/java/com/jsjavaprojects/kafkapractice/CKApplication.java
@@ -2,8 +2,9 @@ package com.jsjavaprojects.kafkapractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//@EnableKafka
-//@EnableKafkaStreams
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+
 @SpringBootApplication
 public class CKApplication {
 
